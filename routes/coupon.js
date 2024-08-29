9d66bb816b3b85bf6aca69f51b3edab761fc0c13
Pawan Kumar Singh <161737976+codecraftsmanpawan@users.noreply.github.com>
2024-08-30 05:04:09 +0530

    up

--- routes/coupon.js
+++ routes/coupon.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../config/multerConfig.js');
-const authenticateToken = require('../middleware/authenticateToken');
-const { createCoupon } = require('../controllers/Coupon/createCoupon.js')
-const { applyCoupon } = require('../controllers/Coupon/applyCoupon.js')
-const { deleteOffer } = require("../controllers/Coupon/deleteOffer.js")
 
+// Import the controller
+const { getActiveCoupons } = require('../controllers/Coupon/getAllCoupon');
 
-
-router.post('/create-coupon', createCoupon); // Route to create coupon by admin and add middleware
-router.post('/apply-code', authenticateToken, applyCoupon); // Route for apply coupon code
+// Route to get all active coupons
+router.get('/allcoupons', getActiveCoupons);  // Ensure route matches expected endpoint
 
 module.exports = router;
